b8ad3664674d0b9fb6ef122de68c950ba7d62dd8
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-12-27 00:33:21 +0100

    Disable `sccache` because of issue with `atom-ide-rust`

--- conf.d/02.environ.fish
+++ conf.d/02.environ.fish
@@ -2,7 +2,7 @@
 set -gx PKG_CONFIG "/usr/bin/pkgconf"
 
 # Rust
-set -x RUSTC_WRAPPER "sccache"
+#set -x RUSTC_WRAPPER "sccache"
 set -x RUST_TARGET_PATH "$RUSTUP_HOME/targets"
 set -x CARGO_INSTALL_ROOT "$HOME/.local"
 
@@ -18,3 +18,6 @@ set -x VISUAL nano
 
 # Path extension
 set -x PATH "$HOME/.local/bin" "$HOME/.local/scripts" "$VITASDK/bin" $PATH
+
+# Fix electron trash command
+set -gx ELECTRON_TRASH "gio"
