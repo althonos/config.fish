0b426e1be8cd490f9cfb770151128cbc20bd93ce
Martin Larralde <martin.larralde@ens-cachan.fr>
2019-03-22 18:42:05 +0100

    Add `docker` command wrapper to lazily start `docker.service`

--- conf.d/03.aliases.fish
+++ conf.d/03.aliases.fish
@@ -4,3 +4,10 @@ alias ds="du -hxd1 . | sort -h"
 alias gvfs-trash="gio trash"
 alias mv="mv -i"
 alias getpass="python -c 'import getpass; print(getpass.getpass())'"
+
+function docker -w docker -d "Start `docker.service` if needed when docker is launched";
+	if test -z (systemctl status docker | grep "Active: active");
+		systemctl start docker
+	end
+	/usr/bin/docker $argv
+end
