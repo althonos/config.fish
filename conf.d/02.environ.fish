41071351d55137bb5274a397f3825386ad5fc2fc
Martin Larralde <martin.larralde@ens-cachan.fr>
2019-03-31 04:33:18 +0200

    Add gem path and ruby binaries to `$PATH`

--- conf.d/02.environ.fish
+++ conf.d/02.environ.fish
@@ -6,6 +6,10 @@ set -gx PKG_CONFIG "/usr/bin/pkgconf"
 set -x RUST_TARGET_PATH "$RUSTUP_HOME/targets"
 set -x CARGO_INSTALL_ROOT "$HOME/.local"
 
+# Ruby
+set -gx GEM_PATH (ruby -r rubygems -e 'puts Gem.user_dir')
+set -gx PATH "$GEM_PATH/bin" $PATH
+
 # Vita SDK
 set -x VITASDK "$XDG_DATA_HOME/vitasdk"
 set -x AR_armv7_vita_eabi "arm-vita-eabi-ar"
