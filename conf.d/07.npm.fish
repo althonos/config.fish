a8c4b65f253d6f284856fa828f0ccdf7352176ef
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-04-01 13:17:57 +0200

    Add configuration to get npm in XDG

---
+++ conf.d/07.npm.fish
@@ -0,0 +1,7 @@
+if [ ! -e "$NPM_CONFIG_USERCONFIG" ];
+	mkdir -p (dirname "$NPM_CONFIG_USERCONFIG")
+	echo "prefix=$XDG_DATA_HOME/npm"                           > "$NPM_CONFIG_USERCONFIG"
+	echo "cache=$XDG_CACHE_HOME/npm"                           > "$NPM_CONFIG_USERCONFIG"
+	echo "tmp=$XDG_RUNTIME_DIR/npm"                            > "$NPM_CONFIG_USERCONFIG"
+	echo "init-module=$XDG_CONFIG_HOME/npm/config/npm-init.js" >"$NPM_CONFIG_USERCONFIG"
+end
